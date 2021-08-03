32b0433ba75ec470f8d74b99f6544c0bddf6637f
petert360 <73585032+petert360@users.noreply.github.com>
2021-08-03 17:49:34 +0200

    update

--- testapp/testapp.js
+++ testapp/testapp.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const app = express();
 
+// ez egy middleware, mert kezeli a req, res objektumokat és
+// tartalmazza  a next fv. hívást, ami gyakorlatilag egy return parancsnak felel meg.
+const timeLogger = (req, res, next) => {
+    console.log(Date.now());
+    next();
+};
+
+const firstMW = (req, res, next) => {
+    console.log('First middleware');
+    next();
+};
+
+const secondMW = (req, res, next) => {
+    console.log('Second middleware');
+    next();
+};
+
+// app.use(timeLogger);
+
+app.use(timeLogger, firstMW, secondMW)
+
 app.get('/test', (req, res, next) => {
-    res.send('hello')
+    res.send('hello');
     console.log('ABC');
 });
 
